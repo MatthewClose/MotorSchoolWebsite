17683e46562a1463ce6873c82b81c183a4e51bd6
Matthew Close <Matthew.Close@oracle.com>
2023-04-20 12:06:55 +0100

    footer and Reasons section test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import "./App.css";
 import About from "./components/About/About";
-import Footer from "./components/Footer/Footer";
-// import Hero from "./components/Hero/Hero";
+// import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
 import Contact from "./components/Contact/Contact";
 import Icons from "./components/Icons/Icons";
-import Reasons from "./components/Reasons/Reasons";
+// import Reasons from "./components/Reasons/Reasons";
 import Testimonials from "./components/Testimonials/Testimonials";
 
 function App() {
   return (
     <div className="App">
-      {/* <Hero /> */}
+      <Hero />
       <Icons />
-      <Reasons />
+      {/* <Reasons /> */}
       <Testimonials />
       <About />
       <Contact />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
