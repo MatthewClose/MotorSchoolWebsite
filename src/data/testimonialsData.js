94d2e30a71d1df6e26f30b810dc7c91c6d77b410
Matthew Close <Matthew.Close@oracle.com>
2023-05-02 20:45:38 +0100

    Updated Testimonial data

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -3,18 +3,18 @@ export const testimonialsData = [
     review:
       "just passed my test having taking lessons with Billy, learning to drive couldnt have been easier with Billy. Always felt confident and relaxed- no stress coming up to lessons or the test. Highly recommend!",
     name: "Aoife M",
-    status: "Belfast",
-  },
-  {
-    review:
-      "I highly recommend billy I first started with no driving experience, never behind a car wheel before . The time and patience's billy shows you each time you get in his car just makes you enjoy learning with him he is always on time and never cancelled a lesson with me I highly recommend him to anyone that wants to learn and you'll 100% get the pass with this man cheers for everything billy",
-    name: "BOB SMITH",
-    status: "Craigavon",
+    status: "Lurgan",
   },
   {
     review:
       "Thank you Billy Hughes for helping me pass my driving test first time. Thanks for the patience and craic lol. Appreciate it all soooo much. Will highly recommend you.👍👍",
     name: "Edel",
+    status: "Craigavon",
+  },
+  {
+    review:
+      "I highly recommend billy I first started with no driving experience, never behind a car wheel before . The time and patience's billy shows you each time you get in his car just makes you enjoy learning with him he is always on time and never cancelled a lesson with me I highly recommend him to anyone that wants to learn and you'll 100% get the pass with this man cheers for everything billy",
+    name: "James",
     status: "Craigavon",
   },
   {
